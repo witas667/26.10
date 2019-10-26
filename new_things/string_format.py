3c4114a39d90b46294d1e1f5e9140b8fb98608c4
Dominik Witas <dominikwitas@pspll119.home>
2019-10-26 11:28:06 +0200

    dodalismy plink new_things w katalogu

---
+++ new_things/string_format.py
@@ -0,0 +1,10 @@
+# x = 5
+# # krotka = ("element", "drugi_element")
+# #
+# # print(f"Nasza liczba to {x} ba ffsd dsfsdf ds {krotka}")
+# # print("Nasza liczba to {} ba ffsd dsfsdf ds {}".format(x+15, krotka))
+
+Name = input()
+Surname = input()
+
+print("twoje imie to {} a twoje naziwsko to {}".format(Name.capitalize(), Surname.capitalize()))
